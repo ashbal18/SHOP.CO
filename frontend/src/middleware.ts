ac704c69145df601fbc2583566afa9c04ecec9d4
ashbal18ts <ashbalfc99@gmail.com>
2025-06-22 23:49:11 +0700

    merge

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
 
   if (!token && req.nextUrl.pathname == "/") {
-    return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
   return NextResponse.next();
