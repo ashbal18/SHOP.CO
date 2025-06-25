500d69ebee6224e10b4917ca128cc0de99face11
ashbal18ts <ashbalfc99@gmail.com>
2025-06-26 02:50:15 +0700

    frontend

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =  "http://localhost:8000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_BE! || "http://localhost:8000/api";
 
 export default axios.create({
   baseURL: BASE_URL,
